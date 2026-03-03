2f6fb300ddd24270090c30cc29ff2e56f7987a1c
MideJoy Akinleye <joyintech50@gmail.com>
2026-03-03 15:10:26 +0100

    mobile responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Header
+const menuLinks = document.querySelector(".menu-links");
+const navLinks = document.querySelector(".nav-links");
+const logo = document.querySelector('.logo-joy');
+const menuButton = document.querySelector('.menu-button');
+
+const showMenu = () => {
+    menuLinks.style.display = 'flex';
+}
+
+const closeMenu = () => {
+    menuLinks.style.display = 'none';
+}
+
+// Contact form submission
 const contactForm = document.getElementById('contact-form');
 
 contactForm.addEventListener('submit', clearForm)
