c6cad46fc206aafc3224e559ef621ee3b6f591ca
MideJoy Akinleye <joyintech50@gmail.com>
2026-03-05 13:37:53 +0100

    project images and preview

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ const showMenu = () => {
 const closeMenu = () => {
     menuLinks.style.display = 'none';
 }
+// Project flip card
+const previewBtn = document.querySelectorAll('.preview-btn');
+const closeBtn = document.querySelectorAll('.close-btn');
+
+previewBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        btn.closest('.flip-container').classList.add('flipped');
+    });
+});
+
+closeBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        btn.closest('.flip-container').classList.remove('flipped');
+    });
+});
 
 // Contact form submission
 const contactForm = document.getElementById('contact-form');
